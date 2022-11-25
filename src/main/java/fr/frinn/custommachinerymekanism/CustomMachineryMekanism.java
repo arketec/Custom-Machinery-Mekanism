a0e37b2daecb1756c92fabf208d7324c62d1897c
Frinn38 <francois.moreau3@aliceadsl.fr>
2022-11-25 13:25:53 +0100

    Setup mod

---
+++ src/main/java/fr/frinn/custommachinerymekanism/CustomMachineryMekanism.java
@@ -0,0 +1,13 @@
+package fr.frinn.custommachinerymekanism;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(CustomMachineryMekanism.MODID)
+public class CustomMachineryMekanism {
+
+    public static final String MODID = "custommachinerymekanism";
+
+    public CustomMachineryMekanism() {
+
+    }
+}
