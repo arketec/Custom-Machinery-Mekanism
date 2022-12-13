9a89690701a819e551fa7098c59dfd4ac3e15073
Frinn38 <francois.moreau3@aliceadsl.fr>
2022-12-13 19:02:42 +0100

    Switch mixin from target to value

--- src/main/java/fr/frinn/custommachinerymekanism/common/mixin/KubeJSIntegrationMixin.java
+++ src/main/java/fr/frinn/custommachinerymekanism/common/mixin/KubeJSIntegrationMixin.java
@@ -1,13 +1,15 @@
 package fr.frinn.custommachinerymekanism.common.mixin;
 
 import fr.frinn.custommachinery.api.integration.kubejs.RecipeJSBuilder;
+import fr.frinn.custommachinery.common.integration.kubejs.CustomCraftRecipeJSBuilder;
+import fr.frinn.custommachinery.common.integration.kubejs.CustomMachineRecipeBuilderJS;
 import fr.frinn.custommachinerymekanism.common.integration.kubejs.GasRequirementJS;
 import fr.frinn.custommachinerymekanism.common.integration.kubejs.InfusionRequirementJS;
 import fr.frinn.custommachinerymekanism.common.integration.kubejs.PigmentRequirementJS;
 import fr.frinn.custommachinerymekanism.common.integration.kubejs.SlurryRequirementJS;
 import org.spongepowered.asm.mixin.Mixin;
 
-@Mixin(targets = {"fr.frinn.custommachinery.common.integration.kubejs.CustomMachineRecipeBuilderJS", "fr.frinn.custommachinery.common.integration.kubejs.CustomCraftRecipeJSBuilder"})
+@Mixin({CustomMachineRecipeBuilderJS.class, CustomCraftRecipeJSBuilder.class})
 public abstract class KubeJSIntegrationMixin implements RecipeJSBuilder,
         GasRequirementJS, InfusionRequirementJS, PigmentRequirementJS, SlurryRequirementJS {
 
